2e77be90189b8de88b2d34e036647031a773b2d7
Nick Cherry <nickcherryjiggz@gmail.com>
2022-06-22 19:03:29 -0400

    fix metamask

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
 import React, { FC } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { Linking, StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+
+interface DeepLinkReplacement {
+  httpsHostAndProtocol: string;
+  appScheme: string;
+}
+
+const deepLinkReplacements: DeepLinkReplacement[] = [
+  {
+    httpsHostAndProtocol: 'https://metamask.app.link/',
+    appScheme: 'metamask://',
+  },
+  // {
+  //   httpsHostAndProtocol: 'https://rnbwapp.com/',
+  //   appScheme: 'rainbow://',
+  // },
+];
 
 const App: FC = () => {
   return (
@@ -10,6 +26,27 @@ const App: FC = () => {
       <WebView
         source={{
           uri: 'https://funny-bombolone-7101cc.netlify.app/',
+        }}
+        onShouldStartLoadWithRequest={(event) => {
+          console.log(event);
+          const deepLinkReplacement = deepLinkReplacements.find(
+            ({ httpsHostAndProtocol }) =>
+              event.url.startsWith(httpsHostAndProtocol),
+          );
+
+          if (deepLinkReplacement) {
+            const targetUrl = decodeURIComponent(
+              event.url.replace(
+                deepLinkReplacement.httpsHostAndProtocol,
+                deepLinkReplacement.appScheme,
+              ),
+            );
+            console.log(`\nattempting to open ${targetUrl}\n`);
+            Linking.openURL(targetUrl);
+            return false;
+          }
+
+          return true;
         }}
       />
     </View>
