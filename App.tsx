056d937348623df2689e3c40b9c65c5bcd265bf5
Nick Cherry <nickcherryjiggz@gmail.com>
2022-06-27 11:30:19 -0400

    remove unnecessary imports

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, Linking, StyleSheet, View } from 'react-native';
+import { Linking, StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const App: FC = () => {
